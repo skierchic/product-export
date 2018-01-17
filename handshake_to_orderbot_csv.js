7ba168bf61d02b5c8b4663b4b2b882b26f8ebe34
Jenna Hight <jennamhight@gmail.com>
2018-01-17 09:21:53 -0500

    Create todaysdate function so orders w/o ship dates are set to today

--- handshake_to_orderbot_csv.js
+++ handshake_to_orderbot_csv.js
@@ -14,13 +14,14 @@ csv()
     "Order Status": "Quote",
     "Customer ID": "LOOKUP CUSTOMER ID",
     "Account ID": jsonObj.customer_id,
-    "Orderguid ID": 840,
+    "Orderguide ID": 840,
     "Product Name": jsonObj.description,
     "Item SKU": jsonObj.sku,
     "Product Quantity": jsonObj.qty,
     "Individual Unit Price": jsonObj.unit_price,
     "Individual Unit Discount": jsonObj.unit_discount,
   }
+  orderbotOrder["Ship Date"] = jsonObj.start_ship_date ? jsonObj.order_date : todaysDate()
   handshake_orders.push(orderbotOrder)
 })
 .on('done', (error) => {
@@ -29,6 +30,13 @@ csv()
   console.log(handshake_orders.length)
 })
 
+let todaysDate = () => {
+  let today = new Date()
+  let d = today.getDate()
+  let m = today.getMonth() + 1
+  let yy = today.getFullYear() - 2000
+  return m + '/' + d + '/' + yy
+}
 // let input = fs.readFileSync(handshake_export_file, 'utf8').trim()
 // console.log(input)
 // let rows = input.split('\n')
